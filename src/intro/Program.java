c7e183fd113cb0d1e5e6ed16814dc50b585fb1ac
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-02-03 19:41:02 +0200

    ex01 done

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -7,30 +7,60 @@ public class Program {
         int decimal = asDecimal("11001101");
 
         System.out.println(decimal); // 205
+
+        String str = asString(decimal);
+
+        System.out.print(str);
     }
 
     public static String asString(int input) {
-        return "";
+        String result = "";
+        while (input > 0) {
+            if (input % 2 == 0){
+                result = '0' + result;
+            }
+            else {
+                result = '1' + result;
+            }
+
+            input /= 2;
+        }
+        return result;
     }
 
     public static int asDecimal(String input) {
         // 11001101
         input = reverse(input);
-        for (int i = 0; i < input.length(); i++) {
 
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '1') {
+                result += pow(2, i);
+            }
         }
 
-        return 0;
+        return result;
     }
 
     private static String reverse(String input) {
         String reversed = "";
-        return "";
+        for (int i = 0; i < input.length(); i++) {
+            reversed = input.charAt(i) + reversed;
+        }
+        return reversed;
     }
 
     private static int pow(int arg, int power) {
         // Java has Math.pow() but this time write your own implementation.
-
-        return 0;
+        if (power == 0){
+            return 1;
+        }
+        else {
+            int result = 1;
+            for (int i = 0; i < power; i++) {
+                result *= arg;
+            }
+            return result;
+        }
     }
 }
