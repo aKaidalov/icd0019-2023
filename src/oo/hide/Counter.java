9346bab79ac4f1ceefeb518a0d5c0db0ade6a217
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-02-25 00:39:59 +0200

    ex04 (1 - 3)

--- src/oo/hide/Counter.java
+++ src/oo/hide/Counter.java
@@ -2,10 +2,27 @@ package oo.hide;
 
 public class Counter {
 
+    private int start;
+    private int step;
+    static boolean firstAttempt = true;
+
     public Counter(int start, int step) {
+        this.start = start;
+        this.step = step;
     }
 
     public int nextValue() {
-        return 0;
+        int result = start;
+        start += step;
+        return result;
     }
+
+//    public int nextValue() {
+//        if (firstAttempt) {
+//            firstAttempt = false;
+//        } else {
+//            start += step;
+//        }
+//        return start;
+//    }
 }
---
+++ src/oo/struct/Point3D.java
@@ -0,0 +1,22 @@
+package oo.struct;
+
+public class Point3D {
+    public Integer x;
+    public Integer y;
+    public Integer z;
+    public Point3D(Integer x, Integer y, Integer z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Integer getX() {
+        return this.x;
+    }
+    public Integer getY() {
+        return this.y;
+    }
+    public Integer getZ() {
+        return this.z;
+    }
+}
