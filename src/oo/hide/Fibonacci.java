decf98889a0c7abd89bdfe4f65d7a7f75c0517cb
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-02-25 00:22:18 +0200

    ex04 (1 - 2)

---
+++ src/oo/hide/Fibonacci.java
@@ -0,0 +1,34 @@
+package oo.hide;
+
+public class Fibonacci {
+
+    private int nMinus1 = 0;
+    private int nMinus2 = 1;
+
+    static boolean firstAttempt = true;
+    static boolean secondAttempt = true;
+
+    public int nextValue() {
+        if (firstAttempt){
+            firstAttempt = false;
+            return 0;
+        } else if (secondAttempt){
+            secondAttempt = false;
+            return 1;
+        } else {
+            int n = nMinus1 + nMinus2;
+            nMinus1 = nMinus2;
+            nMinus2 = n;
+            return n;
+        }
+    }
+// bollee logichn6i variant reshenija
+
+//    public int nextValue() {  // 0, 1, 1, 2, 3, 5, 8...
+//        int result = nMinus2;
+//        int n = nMinus1 + nMinus2;
+//        nMinus1 = nMinus2;
+//        nMinus2 = n;
+//        return result;
+//    }
+}
