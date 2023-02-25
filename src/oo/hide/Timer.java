d892e1f56df65b23d97b9ba35ec726f865cb5403
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-02-25 10:54:50 +0200

    ex04 (1 - 3)

--- src/oo/hide/Timer.java
+++ src/oo/hide/Timer.java
@@ -2,7 +2,11 @@ package oo.hide;
 
 public class Timer {
 
+    public long start = System.currentTimeMillis();
+
     public String getPassedTime() {
+        long difference = System.currentTimeMillis() - start;
+        System.out.println(difference);
         return null;
     }
 }
