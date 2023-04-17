bd3180fe250d515bb20b57f8f15acc391c2f6c16
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-04-18 00:08:37 +0300

    ex10 (1 - 3)

---
+++ src/poly/fruit/Apple.java
@@ -0,0 +1,19 @@
+package poly.fruit;
+
+public class Apple implements Weighable {
+
+    private Double weightInKiloGrams;
+
+    public Apple(Double weight) {
+        this.weightInKiloGrams = weight;
+    }
+
+    @Override
+    public Integer getWeightInGrams() {
+//        Double weightInGrams = weightInKiloGrams * 1000;
+//        return weightInGrams.intValue();
+
+         return Double.valueOf(weightInKiloGrams * 1000).intValue();
+    }
+}
+
