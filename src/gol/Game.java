c68c255c42a586338ccabbff215cc573c1f4dd2a
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-05-07 00:44:59 +0300

    ex11 () ()

--- src/gol/Game.java
+++ src/gol/Game.java
@@ -27,26 +27,59 @@ public class Game {
     }
 
     public void toggle(int x, int y) {
-        if (this.isAlive(x, y)) {
-            board[x][y] = false;
-        } else {
-            board[x][y] = true;
-        }
+        board[y][x] = !this.isAlive(x, y);
     }
 
     public Integer getNeighbourCount(int x, int y) {
-        throw new RuntimeException("not implemented yet");
+        // was inspired by this: https://www.geeksforgeeks.org/find-safe-cells-in-a-matrix/
+        int counter = 0;
+
+        counter += checkCell(y - 1, x - 1, board);
+        counter += checkCell(y - 1, x, board);
+        counter += checkCell(y - 1, x + 1, board);
+        counter += checkCell(y, x + 1, board);
+        counter += checkCell(y + 1, x + 1, board);
+        counter += checkCell(y + 1, x, board);
+        counter += checkCell(y + 1, x - 1, board);
+        counter += checkCell(y, x - 1, board);
+
+        return counter;
+    }
+
+    public int checkCell(int y, int x, boolean[][] matrix) { // return 0 or 1
+        if (y < 0 || x < 0 || y > this.rows || x > this.columns) {
+            return 0;   // if a cell is out of matrix
+        } else if (!matrix[y][x]) {
+            return 0;   // if a cell is in matrix
+        }
+        return 1;
     }
 
     public void nextFrame() {
-        throw new RuntimeException("not implemented yet");
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                int neighborCount = getNeighbourCount(x, y);
+                boolean isLiving = isAlive(x, y);
+                board[y][x] = nextState(isLiving, neighborCount);
+            }
+        }
     }
 
     public void clear() {
-        throw new RuntimeException("not implemented yet");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.board[i][j] = false;
+            }
+        }
     }
 
     public boolean nextState(boolean isLiving, int neighborCount) {
-        throw new RuntimeException("not implemented yet");
+        if (neighborCount < 2 || neighborCount > 3) {
+            return false;
+        } else if (neighborCount == 2) {
+            return isLiving;
+        } else {
+            return true;
+        }
     }
 }
