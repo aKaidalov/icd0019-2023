1b1a10d02a50018f4100815c4c5d0ce31d167bc5
Aleksandr Kaidalov <alkaid@ttu.ee>
2023-05-07 14:33:54 +0300

    ex11 (x1)

--- src/gol/Game.java
+++ src/gol/Game.java
@@ -1,22 +1,14 @@
 package gol;
 
 public class Game {
-
-    private int rows;
-    private int columns;
-    private boolean [][] board;
-
-    public Game() {
-        this.rows = 15;
-        this.columns = 20;
-        this.board = new boolean[rows][columns];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                this.board[i][j] = false;
-            }
-        }
-    }
+    /* There was a problem for me, which I didn't manage to understand.
+    * Why when we crate a Gui object we pass values as number of columns, number of rows and ect,
+    * but in my program rows and columns have opposite spots?
+    * Doesn't it mean when I create rows, they are the same as coordinate Y and columns are coordinate X?
+    */
+    private int rows = 20;
+    private int columns = 15;
+    private boolean [][] board = new boolean[rows][columns];
 
     public void markAlive(int x, int y) {
         board[y][x] = true;
@@ -47,30 +39,38 @@ public class Game {
     }
 
     public int checkCell(int y, int x, boolean[][] matrix) { // return 0 or 1
-        if (y < 0 || x < 0 || y > this.rows || x > this.columns) {
-            return 0;   // if a cell is out of matrix
-        } else if (!matrix[y][x]) {
-            return 0;   // if a cell is in matrix
+        try {
+            if (y < 0 || x < 0 || y > this.rows || x > this.columns) {
+                return 0;   // if a cell is out of matrix
+            } else if (!matrix[y][x]) {
+                return 0;   // if a cell is in matrix, but its value is false
+            }
+            return 1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 0;
         }
-        return 1;
     }
 
     public void nextFrame() {
+        boolean[][] copy = new boolean[rows][columns];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                copy[y][x] = board[y][x];
+            }
+        }
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
                 int neighborCount = getNeighbourCount(x, y);
                 boolean isLiving = isAlive(x, y);
-                board[y][x] = nextState(isLiving, neighborCount);
+                copy[y][x] = nextState(isLiving, neighborCount);
             }
         }
+
+        board = copy;
     }
 
     public void clear() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                this.board[i][j] = false;
-            }
-        }
+        board = new boolean[rows][columns];
     }
 
     public boolean nextState(boolean isLiving, int neighborCount) {
